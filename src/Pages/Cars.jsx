2168fb6d9e307067e23a1c10714979a2390b7120
Hager Tamer <hagertamer16@hotmail.com>
2026-02-14 23:26:49 +0200

    Setup project structure for Hager and Waheed

--- src/Pages/Cars.jsx
+++
@@ -1,3 +0,0 @@
-export default function Cars() {
-  return <div className="p-5"><h1>Our Available Cars 🏎️</h1></div>;
-}
--- src/Pages/Login.jsx
+++
@@ -1,3 +0,0 @@
-export default function Login() {
-  return <div className="p-5"><h1>Login to your account 🔑</h1></div>;
-}
